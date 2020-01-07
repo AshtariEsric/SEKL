eceabdff93887f590c2b9b91211dfb43a7443174
Dennis <dennishasselbusch@icloud.com>
2020-01-07 23:57:49 +0100

    Initial Commit

---
+++ SEKL/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SEKL
+//
+//  Created by Dennis Hasselbusch on 07.01.20.
+//  Copyright © 2020 Dennis Hasselbusch. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
