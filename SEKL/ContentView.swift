1731f0d4e752773a1ca386c8e0ad6591601659c2
Dennis <dennishasselbusch@icloud.com>
2020-07-15 08:00:58 +0200

    Revert "0.62"
    This reverts commit c9d5279f7b51ae5e55faf3be9ef243c7f19c15b2.

--- SEKL/ContentView.swift
+++ SEKL/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView : View {
     //signInSuccess auf True, damit nicht permanent eingeloggt werden muss - muss bei release version zurück auf false geändert werden
-    @State var signInSuccess = true
+    @State var signInSuccess = false
     @EnvironmentObject var session: SessionStore
     
     func getUser(){
