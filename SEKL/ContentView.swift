3d1580396433aae5f6e6c0323c69a5661bdd6b72
Dennis <dennishasselbusch@icloud.com>
2020-11-22 22:09:19 +0100

    documentation 0.5
    basic documentation.

--- SEKL/ContentView.swift
+++ SEKL/ContentView.swift
@@ -10,13 +10,14 @@ import SwiftUI
 struct ContentView : View {
     //signInSuccess auf True, damit nicht permanent eingeloggt werden muss - muss bei release version zurück auf false geändert werden
     @State var signInSuccess = true
-
     @EnvironmentObject var session: SessionStore
     
     func getUser(){
         session.listen()
     }
-    
+    /*
+     Wenn signInSuccess (Login erfolgreich) true ist, wird die EKL geöffnet, sonst wird man auf die LoginFormView weiter geleitet.
+     **/
     var body : some View {
         return Group {
             if signInSuccess {
